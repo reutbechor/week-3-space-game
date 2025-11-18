07484d722b6c97e5f443e30fe660c640ed121ea6
rb <reutbechor@gmail.com>
2025-11-18 02:17:15 +0200

    Delete Assets/Scripts directory

--- Assets/Scripts/4-levels/GAME_STATUS.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-/**
- * This is a static class that keeps the player scores between scenes.
- */
-public class GAME_STATUS: MonoBehaviour {
-    public static int playerScore = 0;
-}
